0419ecf6652531d3110c5b7dbf1e97a43d6d35de
BartoloTarantino <bartolomeotarantino962@gmail.com>
2025-12-03 10:15:23 -0500

    Add files via upload

---
+++ UNIT_1/S2/L3/es_band1.py
@@ -0,0 +1,14 @@
+#Programma che genera il nome di una band tramite input utente
+
+città = input('Inserisci nome città: ')
+animale_domestico = input('Inserisci nome animale domestico: ')
+
+
+
+
+nome_band = città + "" + animale_domestico
+
+print(f"Il nome della tua band potrebbe essere: {nome_band}")
+
+
+
